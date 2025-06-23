cef004b87d691114f5c97b199d6d1ac34418ef3d
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-06-23 10:33:29 +0530

    seo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,11 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext.jsx";
 import {DarkThemeContextProvider} from "./context/DarkTheme.jsx"	
+import {HelmetProvider} from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root")).render(
+
 	<React.StrictMode>
+		<HelmetProvider>
 		<BrowserRouter>
 			<AuthContextProvider>
 				<DarkThemeContextProvider>
@@ -14,5 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				</DarkThemeContextProvider>
 			</AuthContextProvider>
 		</BrowserRouter>
+		</HelmetProvider>
 	</React.StrictMode>
 );
